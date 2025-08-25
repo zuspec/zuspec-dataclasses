f0356388a32cc395ffb3df4a167b594275526a45
Matthew Ballance <matt.ballance@gmail.com>
2025-08-25 01:33:25 +0000

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/zuspec/dataclasses/api/visitor.py
+++ src/zuspec/dataclasses/api/visitor.py
@@ -2,15 +2,21 @@ import dataclasses as dc
 from typing import Callable, ClassVar, Dict, Type
 from ..annotation import Annotation
 from ..component import Component
+from ..ports import Input, Output
 from ..struct import Struct
 
 @dc.dataclass
 class Visitor(object):
     _type_m : Dict[Type,Callable] = dc.field(default_factory=dict)
+    _field_factory_m : Dict[Type,Callable] = dc.field(default_factory=dict)
 
     def __post_init__(self):
         self._type_m = {
             Component : self.visitComponentType
+        }
+        self._field_factory_m = {
+            Input : self.visitInput,
+            Output : self.visitOutput
         }
 
     def visit(self, t):
@@ -34,13 +40,25 @@ class Visitor(object):
         for f in dir(t):
             o = getattr(t, f)
             if callable(o) and hasattr(o, Annotation.NAME):
+                self.visitExec(f, o)
                 print("Found")
 
+    def visitExec(self, name, m):
+        pass
+
     def _dispatchField(self, f : dc.Field):
-        self.visitField(f)
+        if f.default_factory in self._field_factory_m.keys():
+            self._field_factory_m[f.default_factory](f)
+        else:
+            self.visitField(f)
 
     def visitField(self, f : dc.Field):
         pass
 
     def visitInput(self, f : dc.Field):
+        self.visitField(f)
+        pass
+
+    def visitOutput(self, f : dc.Field):
+        self.visitField(f)
         pass
