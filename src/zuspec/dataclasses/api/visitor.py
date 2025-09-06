0bf964f3203fd2d9ec801a47cac1b7de8377f56c
Matthew Ballance <matt.ballance@gmail.com>
2025-09-06 16:11:02 +0000

    pre-merge
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/zuspec/dataclasses/api/visitor.py
+++ src/zuspec/dataclasses/api/visitor.py
@@ -1,6 +1,7 @@
 import dataclasses as dc
 from typing import Callable, ClassVar, Dict, Type
 from ..annotation import Annotation
+from ..bit import Bit
 from ..component import Component
 from ..struct import Struct
 
@@ -27,20 +28,52 @@ class Visitor(object):
         self.visitStructType(t)
         pass
 
-    def visitStructType(self, t : Struct):
+    def _visitFields(self, t : Struct):
+        print("--> visitFields")
         for f in dc.fields(t):
+            print("Field: %s" % f.name)
             self._dispatchField(f)
+        print("<-- visitFields")
+
+    def _dispatchField(self, f : dc.Field):
+        if f.default_factory is not None:
+            if issubclass(f.default_factory, input):
+                self.visitInputField(f)
+            elif issubclass(f.default_factory, Output):
+                self.visitOutputField(f)
+        else:
+            if f.type == int:
+                self.visitIntField(f)
+            elif f.type == str:
+                self.visitStrField(f)
+            elif callable(f.type):
+                if issubclass(f.type, Bit):
+                    visitBitField(f)
+                print("class")
+            else:
+                print("Error: unhandled: %s" % str(f.type))
+
+    def visitStructType(self, t : Struct):
+        self._visitFields(t)
         
         for f in dir(t):
             o = getattr(t, f)
             if callable(o) and hasattr(o, Annotation.NAME):
                 print("Found")
 
-    def _dispatchField(self, f : dc.Field):
+    def visitField(self, f : dc.Field): pass
+
+    def visitInputField(self, f : dc.Field):
         self.visitField(f)
 
-    def visitField(self, f : dc.Field):
-        pass
+    def visitOutputField(self, f : dc.Field):
+        self.visitField(f)
+
+    def visitIntField(self, f : dc.Field):
+        self.visitField(f)
+
+    def visitStrField(self, f : dc.Field):
+        self.visitField(f)
 
     def visitInput(self, f : dc.Field):
         pass
