5b85cef60fb552c739d0879c3cdf7f204bab4af5
Matthew Ballance <matt.ballance@gmail.com>
2025-09-10 02:04:55 +0000

    Update license header
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/zuspec/dataclasses/timebase.py
+++ src/zuspec/dataclasses/timebase.py
@@ -1,3 +1,18 @@
+#****************************************************************************
+# Copyright 2019-2025 Matthew Ballance and contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#****************************************************************************
 import abc
 from typing import TYPE_CHECKING
 from .decorators import dataclass, input
@@ -29,4 +44,3 @@ class TimeBase(object):
 #class TimeBaseSignal(TimeBase,Component):
 #    clock : Bit = input()
 #    reset : Bit = input()
-
